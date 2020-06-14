dd004f1d6f6650e147986999ac478e5ba92de73a
akira <>
2020-06-14 22:19:40 +0300

    new logic in progress add operator func

--- main.py
+++ main.py
@@ -20,39 +20,79 @@ def argument_1():
 	return arg_1
 
 def operator(a, b):
+
 	number = (1,2,3,4)
+	print('Какой знак выберешь?: \n 1: + \n 2: - \n 3: * \n 4: / ')
+
 	while 1:
 		index = input()
 		try:
 			index = int(index)
 			if index in number:
+
 				if index == 1 :
 					rez = a + b
+					print(a, '+' , b)
 					return rez
 				elif index == 2 :
 					rez = a - b
+					print(a, '-' , b)
 					return rez
 				elif index == 3 :
 					rez = a * b
+					print(a, '*' , b)
 					return rez
 				elif index == 4 :
 					rez = a / b
+					print(a, '/' , b)
 					return rez
+
 			break
 		except ValueError:
-			print('Vvedite 1, 2, 3, 4')
+			print('Введите: 1, 2, 3, 4')
 			continue
 
 def main():
 	while True:
 		a = argument_1()
-		b = argument_1()
 
 		print('Введено: {}, {}'.format(a, b))
-
-		print('Какой знак выберешь?: \n 1: +\n 2: -\n 3: *\n 4: / ')
+		b = argument_1()
+		print('Какой знак выберешь?: \n 1: + \n 2: - \n 3: * \n 4: / ')
 		operation = operator(a,b)
 		print('Результат: ', operation)
 
+def logick_2():
+	while True:
+		a = argument_1()
 
-main()
+		b = argument_1()
+
+		rezult = operator (a,b)
+		print('Результат: ', rezult)
+
+
+def operator_2():
+	oper = {1: '-', 2 : '+', 3 : '*', 4: '/'}
+	while 1:
+		print('Выбери число',list(oper.keys()))
+		number = input()
+		try:
+			number = int(number)
+			if	number in list(oper.keys()):
+				print(oper.get(number))
+
+			elif number not in list(oper.keys()):
+				print('is not verifer number')
+			continue
+			break
+		except (ValueError , AttributeError):
+			print(' Введите число! ')
+			continue
+
+
+
+
+operator_2()
+# logick_2()
+# main()
