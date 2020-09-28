2cbeeaf1947a2b1a80a20e65b9b7413a7f2438bd
akira droid <linkomizin@gmail.com>
2020-09-28 13:58:52 +0300

    исправил логику. убрал ненужное.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print('Привет, это калькулятор')
 
-
-
 def argument_1():
 
 	while 1:
@@ -19,72 +17,49 @@ def argument_1():
 
 	return arg_1
 
-def operator(a, b):
+def operator(a, b, index):
 
-	number = (1,2,3,4)
-	print('Какой знак выберешь?: \n 1: + \n 2: - \n 3: * \n 4: / ')
+	if index == 1 :
+		rez = a + b
+		print(a, '+' , b)
+		return rez
+	elif index == 2 :
+		rez = a - b
+		print(a, '-' , b)
+		return rez
+	elif index == 3 :
+		rez = a * b
+		print(a, '*' , b)
+		return rez
+	elif index == 4 :
+		rez = a / b
+		print(a, '/' , b)
+		return rez
 
-	while 1:
-		index = input()
-		try:
-			index = int(index)
-			if index in number:
 
-				if index == 1 :
-					rez = a + b
-					print(a, '+' , b)
-					return rez
-				elif index == 2 :
-					rez = a - b
-					print(a, '-' , b)
-					return rez
-				elif index == 3 :
-					rez = a * b
-					print(a, '*' , b)
-					return rez
-				elif index == 4 :
-					rez = a / b
-					print(a, '/' , b)
-					return rez
-
-			break
-		except ValueError:
-			print('Введите: 1, 2, 3, 4')
-			continue
-
-def main():
-	while True:
-		a = argument_1()
-
-		
-		b = argument_1()
-
-		print('Введено: {a}, {b}'.format(a, b))
-		print('Какой знак выберешь?: \n 1: + \n 2: - \n 3: * \n 4: / ')
-		operation = operator(a,b)
-		print('Результат: ', operation)
 
 def logick_2():
 	while True:
 		a = argument_1()
+		
 		oper = operator_2()
 		b = argument_1()
+		rezult = operator(a, b, oper[0])
+		print('Результат: ', a, oper[1], b, '=', rezult )
 		
-		
-		print('Результат: ', a, oper, b)#.format(oper)
-
 
 def operator_2():
 	oper = {1: '-', 2 : '+', 3 : '*', 4: '/'}
 	while 1:
+		print(oper)
 		print('Выбери число',list(oper.keys()))
 		number = input()
 		try:
 			number = int(number)
 			if	number in list(oper.keys()):
-				print(oper.get(number))
+				print(oper.get(number)) 
 				znak = oper.get(number)
-				return number and znak
+				return number, znak
 				continue
 			elif number not in list(oper.keys()):
 				print('is not verifer number')
